0dd6696d3b0ff07c88c5434c1c6e1a3dcf17b178
emilio aguirre vicentin <93147546+emilioemiav@users.noreply.github.com>
2022-09-13 19:12:37 -0300

    Add files via upload
    ejercicio de listas y declaracion de variables y constantes

---
+++ listasyvar.js
@@ -0,0 +1,22 @@
+
+var nombre = "emilio";
+console.log (nombre);
+
+var edad = "28";
+console.log(edad)
+
+var des = "true";
+console.log(des)
+
+const fechanac = new Date ("1994,25,june");
+console.log(fechanac)
+
+const libro = {
+    titulo: "el tunel" ,
+    autor: "Ernesto Sabato",
+     fecha: 1960 ,
+     url: "www.asdasd.com"
+}
+
+const lista = [nombre,edad,des,fechanac,libro];
+console.log (lista)
